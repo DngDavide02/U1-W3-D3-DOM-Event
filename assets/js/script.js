7735ca0115d18dc5b4b8240312f86c66a7b1e957
DngDavide02 <dangelodavide02@gmail.com>
2025-03-26 16:30:57 +0100

    finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
 document.getElementById("button").addEventListener("click", function () {
   const input = document.getElementById("inputText");
-  const attivita = input.ariaValueMax.trim();
+  const attivita = input.value.trim(); // Usa .value per ottenere il testo inserito
 
   if (attivita !== "") {
     const lista = document.getElementById("listaAttivita");
+
     const li = document.createElement("li");
     li.textContent = attivita;
+
     const deleteButton = document.createElement("button");
-    deleteButton.textContent = "elimina";
+    deleteButton.textContent = "❌";
     deleteButton.className = "deleteButton";
+
     deleteButton.addEventListener("click", function () {
-      deleteButton.removeChild1("li");
+      li.remove();
     });
-    li.appendChild("deleteButton");
-    lista.appendChild("li");
+
+    li.appendChild(deleteButton);
+    lista.appendChild(li);
+
     input.value = "";
   }
 });
